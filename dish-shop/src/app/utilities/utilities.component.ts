d43689489d92b5738c94f6906b35290be3a7b70c
ununch0r <bodiastel123@gmail.com>
2021-04-11 14:41:57 +0300

    DS-54 Fix alignment

--- dish-shop/src/app/utilities/utilities.component.ts
+++ dish-shop/src/app/utilities/utilities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-utilities',
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./utilities.component.css']
 })
 export class UtilitiesComponent implements OnInit {
+  producerForm : FormGroup;
+  countryForm : FormGroup;
+  cityForm : FormGroup;
+  characteristicForm : FormGroup;
+  valueTypeForm : FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initFroms();
+  }
+
+  initFroms(){
+    this.initProducerForm();
+    this.initCityForm();
+    this.initCountryForm();
+    this.initValueTypeForm();
+    this.initCharacteristicForm();
+  }
+
+  initProducerForm(){
+    this.producerForm = new FormGroup({
+      'name' : new FormControl(null, [Validators.required]),
+      'countryId' : new FormControl(null, [Validators.required])
+    });
+  }
+
+  initCityForm(){
+    this.cityForm = new FormGroup({
+      'name' : new FormControl(null, [Validators.required]),
+      'countryId' : new FormControl(null, [Validators.required])
+    });
+  }
+
+  initCountryForm(){
+    this.countryForm = new FormGroup({
+      'name' : new FormControl(null, [Validators.required]),
+    });
+  }
+
+  initValueTypeForm(){
+    this.valueTypeForm = new FormGroup({
+      'type' : new FormControl(null, [Validators.required]),
+    });
+  }
+
+  initCharacteristicForm(){
+    this.characteristicForm = new FormGroup({
+      'name' : new FormControl(null, [Validators.required]),
+      'valueTypeId' : new FormControl(null, [Validators.required])
+    });
   }
 
 }
